c37a5fd1946b7a7a8a926d73b8ce53517f0819fa
jorgeSilis <84548055+jorgeSilis@users.noreply.github.com>
2023-03-05 21:53:14 -0600

    onboarding&themes

--- pmsna1/lib/provider/theme_provider.dart
+++ pmsna1/lib/provider/theme_provider.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/settings/styles_settings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider with ChangeNotifier{
-
+class ThemeProvider with ChangeNotifier {
   ThemeData? _themeData;
 
-  ThemeProvider(BuildContext context){
-    _themeData=StylesSettings.lightTheme(context);
+  ThemeProvider(BuildContext context) {
+    SharedPreferences.getInstance().then((prefs) {
+      final themeName = prefs.getString('theme');
+      if (themeName != null) {
+        setThemeData(themeName, context);
+      }
+    });
   }
 
   getThemeData() => this._themeData;
-  setThemeData(ThemeData theme){
-    this._themeData = theme;
+
+  Future<void> setThemeData(String? themeName, BuildContext context) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    switch (themeName) {
+      case 'dark':
+        _themeData = StylesSettings.darkTheme(context);
+        await sharedPreferences.setString('theme', 'dark');
+        break;
+      case 'light':
+        _themeData = StylesSettings.lightTheme(context);
+        await sharedPreferences.setString('theme', 'light');
+        break;
+      case 'chill':
+        _themeData = StylesSettings.chillTheme(context);
+        await sharedPreferences.setString('theme', 'chill');
+        break;
+      default:
+        _themeData = StylesSettings.lightTheme(context);
+        await sharedPreferences.setString('theme', 'light');
+        break;
+    }
+
     notifyListeners();
   }
-
-
 }
